cbee2cc3654b2bbcfe69f9253e578a67b112be4b
cabreraleja <alejandra.cabrera796@qmail.cuny.edu>
2024-11-18 18:45:53 -0500

    Initial commit

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function Page()  {
+  return (
+    <main classname="text-center">
+       <h2> classname="text-3xl"> There was a problem.</h2>
+       <p>Page not found. </p>
+       
+       </main>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <main>Acc 343 Exam 2</main>
+    </>
+  );
+}
